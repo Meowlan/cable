c5dcdbb2d44cdfd110b731800ab5baa129fe3342
Meowlan <Meowlan.private@gmail.com>
2023-12-19 05:57:02 +0100

    Tested, Modified, and fixed event core!

--- lua/entities/gmod_wire_expression2/core/custom/events.lua
+++ lua/entities/gmod_wire_expression2/core/custom/events.lua
@@ -1,6 +1,4 @@
-local Clamp = math.Clamp
-
-E2Lib.RegisterExtension("cable-events", true, "base64 conversion helpers")
+E2Lib.RegisterExtension("cable-events", true, "Some helper functions for event managers.")
 
 local function IsEventManager(Player)
     if not IsValid(Player) then return false end
@@ -12,107 +10,117 @@ local function IsEventManager(Player)
     return false
 end
 
-e2function entity:Print(string text)
+e2function void entity:printEvent(string text)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:ChatPrint(text)
 end
 
-e2function entity:Hint(string text, duration)
+e2function void entity:hintEvent(string text, duration)
+    if not IsEventManager(self.player) then return end
+    if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
+
+    WireLib.AddNotify(this, text, NOTIFY_GENERIC, duration)
+end
+
+e2function void entity:setPosEvent(vector pos)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
 
-    WireLib.AddNotify(this, text, NOTIFY_GENERIC, Clamp(duration, 0.7, 7))
+    this:SetPos(pos)
 end
 
-e2function entity:SetPos(vector pos)
-    if not IsEventManager(self.player) then return end
-    if not IsValid(this) then return end
-
-    this:SetPos(text)
-end
-
-e2function entity:SetAng(angle ang)
+e2function void entity:setAngEvent(angle ang)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
 
     this:SetAngles(ang)
 end
 
-e2function entity:Kill()
+e2function void entity:killEvent()
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:Kill()
 end
 
-e2function entity:KillSilent()
+e2function void entity:killSilentEvent()
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:KillSilent()
 end
 
-e2function entity:StripWeapons()
+e2function void entity:stripWeaponsEvent()
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:StripWeapons()
 end
 
-e2function entity:StripWeapon(string text)
+e2function void entity:stripWeaponEvent(string text)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:StripWeapon(text)
 end
 
-e2function entity:GiveWeapon(string text)
+e2function void entity:giveWeaponEvent(string text)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
-    this:GiveWeapon(text)
+    this:Give(text)
 end
 
-e2function entity:SetModel(string text)
-    if not IsEventManager(self.player) then return end
-    if not IsValid(this) then return end
-
-    this:SetModel(text)
-end
-
-e2function entity:SetModel(string text)
+e2function void entity:setModelEvent(string text)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
 
     this:SetModel(text)
 end
 
-e2function entity:SetColor(vector color)
+e2function void entity:setMaterialEvent(string text)
+    if not IsEventManager(self.player) then return end
+    if not IsValid(this) then return end
+
+    this:setMaterial(text)
+end
+
+e2function void entity:setColorEvent(vector color)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
 
     this:SetColor(color)
 end
 
-e2function entity:SetHealth(int health)
+e2function void entity:setHealthEvent(number health)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:SetHealth(health)
 end
 
-e2function entity:SetArmor(int armor)
+e2function void entity:setArmorEvent(number armor)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:SetArmor(armor)
 end
 
-e2function entity:SetJob(int job)
+e2function void entity:setJobEvent(number job)
     if not IsEventManager(self.player) then return end
     if not IsValid(this) then return end
+    if not this:IsPlayer() then return end
 
     this:SetJob(job)
 end
